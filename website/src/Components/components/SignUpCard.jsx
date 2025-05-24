75bc99069967a2f4d2160c092967fef967ede5db
Aren Vanyan <i21.vanyan.aren@etud.ufar.am>
2025-05-24 15:54:21 +0400

    feat: Update SignInCard and SignUpCard components to use light theme styles

--- website/src/Components/components/SignUpCard.jsx
+++ website/src/Components/components/SignUpCard.jsx
@@ -28,7 +28,7 @@ const Card = styled(MuiCard)(({ theme }) => ({
   [theme.breakpoints.up('sm')]: {
     width: '450px',
   },
-  ...theme.applyStyles('dark', {
+  ...theme.applyStyles('light', {
     boxShadow:
       'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
   }),
