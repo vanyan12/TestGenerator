d784c5dc65c9fa39e14b34464acbda29e582fd03
Aren Vanyan <i21.vanyan.aren@etud.ufar.am>
2025-05-27 01:36:20 +0400

    .

--- website/src/Components/AuthContext.jsx
+++ website/src/Components/AuthContext.jsx
@@ -12,9 +12,9 @@ export const AuthProvider = ({ children }) => {
     credentials: "include",
   })
   .then(async (res) => {
-    console.log(res.ok)
     setIsAuth(res.ok)
     const response = await res.json();
+    console.log(response);
     setUser(response.user);
   }
     )
